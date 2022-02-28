f9bf725bdcd6aee53ba5cfe3ab3cb3402c081483
lukasz.gryzbon <lukasz.gryzbon@integreety.co.uk>
2022-02-28 08:43:25 +0000

    fix: Add additional constructor to one of the API classes to make it easier to use

--- api/src/main/kotlin/com/lsdconsulting/exceptionhandling/api/ErrorResponse.kt
+++ api/src/main/kotlin/com/lsdconsulting/exceptionhandling/api/ErrorResponse.kt
@@ -7,4 +7,8 @@ data class ErrorResponse(
     val messages: List<String> = listOf(),
     val dataErrors: List<DataError> = listOf(),
     val attributes: Map<String, Any?> = mapOf()
-) : Serializable
+) : Serializable {
+
+    constructor(errorCode: String? = null, message: String, dataErrors: List<DataError> = listOf(), attributes: Map<String, Any?> = mapOf()):
+            this(errorCode, listOf(message), dataErrors, attributes)
+}
